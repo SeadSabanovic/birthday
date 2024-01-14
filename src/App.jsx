e6538e4cff45e59317ec2d7973d916314191102a
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2024-01-14 20:50:01 +0100

    Add confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import ReactLenis from '@studio-freight/react-lenis'
-import './App.scss'
-import PreLoader from './components/PreLoader'
+import ReactLenis from "@studio-freight/react-lenis";
+import "./App.scss";
+// import PreLoader from './components/PreLoader'
 
 function App() {
-
   return (
     <ReactLenis root>
-      <PreLoader />
+      {/* <PreLoader /> */}
+      <div className="container">
+        <h1>Mali znak pažnje...</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam
+          quia doloribus necessitatibus eum soluta commodi provident distinctio
+          tenetur illo.
+        </p>
+      </div>
     </ReactLenis>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -1,22 +1,36 @@
 import gsap from "gsap";
 import { useLayoutEffect } from "react";
+import confetti from "canvas-confetti";
 
 export default function PreLoader() {
+  const handleConfetti = () => {
+    confetti({
+      particleCount: 250,
+      spread: 120,
+    });
+  };
+
   useLayoutEffect(() => {
     const tl = gsap.timeline();
 
     tl.from(".pre-loader__count", {
+      opacity: 0,
+      scale: 0.8,
+      rotate: 10,
       textContent: 0,
-      duration: 4,
+      duration: 3,
       ease: "expoScale(0.5,7,none)",
       snap: { textContent: 1 },
       stagger: 1,
+      onComplete: () => handleConfetti(),
+    }).to(".pre-loader", {
+      delay: 2,
+      autoAlpha: 0,
     });
   });
 
   return (
     <div className="pre-loader">
-      <h1 className="pre-loader__title">ELMA</h1>
       <h1 className="pre-loader__count">25</h1>
     </div>
   );
